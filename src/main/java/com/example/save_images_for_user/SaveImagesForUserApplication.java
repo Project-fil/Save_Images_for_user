078d8fb411037fac0985960ec9c168981d3c5c7f
Andryiliy <andryfil@wizardsdev.com>
2022-04-08 18:08:41 +0300

    initial commit

---
+++ src/main/java/com/example/save_images_for_user/SaveImagesForUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.save_images_for_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveImagesForUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveImagesForUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/save_images_for_user/SaveImagesForUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.save_images_for_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveImagesForUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
